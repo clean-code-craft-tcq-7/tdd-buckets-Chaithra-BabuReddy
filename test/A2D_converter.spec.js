dac9aef2257a51689ff4d10b3160637b04211cee
Chaithra Babu Reddy <BabuReddy.Chaithra@in.bosch.com>
2023-02-20 18:47:50 +0530

    extended a2d converter functionality

--- test/A2D_converter.spec.js
+++ test/A2D_converter.spec.js
@@ -9,20 +9,20 @@ describe('A2D_converter tests', () => {
         assert.throws(function () { bitConverter.a2dConverter(inputs, 10) }, Error, 'Error-invalid reading');
     });
     it('should return converted values', () => {
-        const inputs = [0, 128, 256, 512, 700, 1024, 1146, 2048];
+        const inputs = [0, 128, 256, 512, 1024, 1146, 2048];
         let res = bitConverter.a2dConverter(inputs, 10);
-        expect(res).to.eql([0, 0, 1, 1, 2, 3, 3, 5]);
+        expect(res).to.eql([0, 0, 1, 1, 3, 3, 5]);
     });
 });
 
 describe('integration test', () => {
-    it('should return error message for input 4096', () => {
-        const inputs = [0, 128, 256, 4096, 4098];
+    it('should return error for invalid inputs', () => {
+        const inputs = [0, 4096, 4098];
         assert.throws(function () {
             currentRange.getRangeOfCurrents(bitConverter.a2dConverter(inputs, 10))
         }, Error, 'Error-invalid reading');
     });
-    it('should return converted values', () => {
+    it('should return range of values', () => {
         const inputs = [0, 128, 256, 512, 700, 1024, 1146, 2048];
         let res = currentRange.getRangeOfCurrents(bitConverter.a2dConverter(inputs, 10));
         expect(res).to.eql(['{0-3:7}', '{5-5:1}']);
